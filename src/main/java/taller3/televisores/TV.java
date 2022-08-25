196b31b602d68b3099bbb59dc9599ce1469d500d
Daniel Estrada <daestradam@unal.edu.co>
2022-08-25 01:09:30 -0500

    Clases

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,97 @@
+package taller3.televisores;
+
+public class TV {
+	private Marca marca;
+	private int canal=1;
+	private int precio=500;
+	private boolean estado;
+	private int volumen=1;
+	private Control control;
+	private static int numTV;
+	
+	public TV(Marca marca, boolean estado) {
+		this.marca=marca;
+		this.estado=estado;
+		numTV++;
+	}
+	
+	public void setMarca(Marca marca) {
+		this.marca=marca;
+	}
+	
+	public Marca getMarca() {
+		return marca;
+	}
+	
+	public void setControl(Control control) {
+		this.control=control;
+	}
+	
+	public Control getControl() {
+		return control;
+	}
+	
+	public void setPrecio(int precio) {
+		this.precio=precio;
+	}
+	
+	public int getPrecio() {
+		return precio;
+	}
+	
+	public void setVolumen(int volumen) {
+		this.volumen=volumen;
+	}
+	
+	public int getVolumen() {
+		return volumen;
+	}
+	
+	public void setCanal(int canal) {
+		this.canal=canal;
+	}
+	
+	public int getCanal() {
+		return canal;
+	}
+	
+	public static int getNumTV() {
+		return numTV;
+	}
+	
+	public void turnOn(){
+		estado=true;
+	}
+	
+	public void turnOff(){
+		estado=false;
+	}
+	
+	public boolean getEstado() {
+		return estado;
+	}
+	
+	public void canalUp(){
+		if(estado=true && canal<120) {
+			canal++;
+		}
+	}
+	
+	public void canalDown(){
+		if(estado=true && canal>1) {
+			canal--;
+		}
+	}
+	
+	public void volumenUp(){
+		if(estado=true && volumen<7) {
+			volumen++;
+		}
+	}
+	
+	public void volumenDown(){
+		if(estado=true && volumen>0) {
+			volumen--;
+		}
+	}
+}
